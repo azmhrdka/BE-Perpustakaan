bce97e091b27c1b3672d0aa9832ef14f38bb3186
azmhrdka <moch._mahardika_33rpl@student.smktelkom-mlg.sch.id>
2026-04-07 09:04:03 +0700

    fix: use JWT_SECRET from env

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,17 +1,17 @@
 import { PassportStrategy } from '@nestjs/passport'; 
-import { Strategy, ExtractJwt } from 'passport-jwt'; 
-import { Injectable } from '@nestjs/common'; 
-@Injectable() 
-export class JwtStrategy extends 
-PassportStrategy(Strategy) { 
-    constructor() { 
-        super({ 
-            jwtFromRequest: 
-ExtractJwt.fromAuthHeaderAsBearerToken(), 
-            secretOrKey: 'SECRET_KEY', 
-        }); 
-    } 
-    validate(payload: any) { 
-    return payload; 
-    } 
+import { Strategy, ExtractJwt } from 'passport-jwt';
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+    constructor(private configService: ConfigService) {
+        super({
+            jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+            secretOrKey: configService.get('JWT_SECRET') || 'SECRET_KEY',
+        });
+    }
+    validate(payload: any) {
+        return payload;
+    }
 }
